d74d59921d9223ee08618ad437a4de53c5f11b0a
ilyasfet <ilyas.fet@gmail.com>
2023-08-16 16:11:22 +0100

    user modifications

--- src/main/java/com/iprojects/openrev/entity/User.java
+++ src/main/java/com/iprojects/openrev/entity/User.java
@@ -1,25 +1,35 @@
 package com.iprojects.openrev.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Entity
-@Table(name = "User")
+@Table(name = "Users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "first_name")
+    private String firstName;
 
-    @Column(name = "surName")
-    private String surName;
+    @Column(name = "last_name")
+    private String lastName;
 
     @Column(name = "age")
     private String age;
 
-    @Column(name = "userName")
+    @Column(name = "user_name")
     private boolean userName;
 
     @Column(name = "email")
@@ -28,7 +38,28 @@ public class User {
     @Column(name = "password")
     private boolean password;
 
-    public User(){
+    @Column(name = "title")
+    private boolean title;
 
-    }
+    @Column(name = "phone")
+    private boolean phone;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Column(name = "modified_date")
+    private Date modifiedDate;
+
+    @Column(name = "using_mfa")
+    private boolean usingMfa;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @Column(name = "locked")
+    private boolean locked;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
 }
