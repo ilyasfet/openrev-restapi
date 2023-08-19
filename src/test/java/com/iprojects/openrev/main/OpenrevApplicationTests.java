449cba14b96781fe6d98579efba1004ebd116922
ilyasfet <ilyas.fet@gmail.com>
2023-08-20 00:13:31 +0100

    add some files, app configuration for sping and profiles

--- src/test/java/com/iprojects/openrev/main/OpenrevApplicationTests.java
+++ src/test/java/com/iprojects/openrev/main/OpenrevApplicationTests.java
@@ -2,8 +2,10 @@ package com.iprojects.openrev.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"test"})
 class OpenrevApplicationTests {
 
 	@Test
