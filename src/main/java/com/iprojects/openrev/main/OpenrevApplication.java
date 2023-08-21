1fdec5c67c09ef607251da336f262ee8120605ec
ilyasfet <ilyas.fet@gmail.com>
2023-08-22 00:43:03 +0100

    adding security and actuator and controller for test

--- src/main/java/com/iprojects/openrev/main/OpenrevApplication.java
+++ src/main/java/com/iprojects/openrev/main/OpenrevApplication.java
@@ -1,5 +1,6 @@
 package com.iprojects.openrev.main;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.iprojects.openrev")
 public class OpenrevApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(OpenrevApplication.class, args);
 	}
--- src/main/java/com/iprojects/openrev/rest/UserController.java
+++ src/main/java/com/iprojects/openrev/rest/UserController.java
@@ -1,13 +1,17 @@
 package com.iprojects.openrev.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Value("${custom.value.name}")
+    private String valueFromProFile;
     @GetMapping ("/")
     public String getUsers(){
+        System.out.println(valueFromProFile);
         return "userss";
     }
 
